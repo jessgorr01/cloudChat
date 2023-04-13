4ea736ef8968cc0c9de5a845f5c8615889b09f13
Brendan <85203863+brdxn@users.noreply.github.com>
2023-04-13 10:36:05 -0400

    Update worker.py

--- ramBot/worker/worker.py
+++ ramBot/worker/worker.py
@@ -1,1 +1,10 @@
 #this will be the worker file to connect all the messages
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="cloud",
+  password="cloudroot"
+)
+
+print(mydb)
