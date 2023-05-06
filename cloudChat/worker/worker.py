95bc2a332bfedfe422436b841c835922d7a40feb
Brendan <85203863+brdxn@users.noreply.github.com>
2023-05-06 14:28:47 -0400

    Update worker.py

--- cloudChat/worker/worker.py
+++ cloudChat/worker/worker.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import re
+import re 
 
 app = Flask(__name__)
 
